6e075cbae4d70f5d898752022b42eba4dcc49ce3
Ethan Wilk <ethanwilk@gmail.com>
2019-09-29 16:21:03 -0700

    Update update.js

--- scripts/update.js
+++ scripts/update.js
@@ -1,2 +1,18 @@
-alert("hi");
-document.getElementById("u412-4").textContent="newtext";
+function test(){
+    var xmlhttp = new XMLHttpRequest();
+    var url = "https://api.coindesk.com/v1/bpi/currentprice.json";
+    xmlhttp.onreadystatechange = function() {
+      if (this.readyState == 4  &&  this.status == 200) {
+        var json = JSON.parse(this.responseText);
+        parseJson(json);
+      }		
+    };
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+    function parseJson(json) {
+      var time = "<b>Last Updated : " + json["time"]["updated"] + "</b>";
+      var usdValue = "1 BTC equals to $" + json["bpi"]["USD"]["rate"];
+      var gbpValue = "1 BTC equals to &pound;" + json["bpi"]["GBP"]["rate"];
+      var euroValue = "1 BTC equals to &euro;" + json["bpi"]["EUR"]["rate"];
+      console.log(usdValue);
+      document.getElementById("u412-4").textContent=usdValue;}}
