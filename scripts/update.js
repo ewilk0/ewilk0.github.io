6bc1e7bde9c71c13df82a692a9349c6b03126ccf
Ethan Wilk <ethanwilk@gmail.com>
2019-09-29 21:15:37 -0700

    Update update.js

--- scripts/update.js
+++ scripts/update.js
@@ -5,8 +5,7 @@ function updatePrice(){
 
   ws.onmessage = function(msg){
     var jsonObject = JSON.parse(msg.data);
-    var newFloat = parseFloat(jsonObject.c);
-    var finalPrice = Number((newFloat).toFixed(2));
+    var finalPrice = jsonObject.c.toFixed(2);
     if(finalPrice > lastPrice){
       document.getElementById("u412-4").style.color = 'green';
       document.getElementById("u412-4").style.fontFamily = 'Courier';
