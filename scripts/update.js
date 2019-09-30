9bc728a9ae35e8f22da1d2d94dc59ca20fc92c62
Ethan Wilk <ethanwilk@gmail.com>
2019-09-29 21:36:59 -0700

    Create update.js

---
+++ scripts/update.js
@@ -0,0 +1,24 @@
+var lastPrice = 0;
+
+function updatePrice(){
+  var ws = new WebSocket('wss://stream.binance.com:9443/ws/btcusdt@miniTicker');
+
+  ws.onmessage = function(msg){
+    var jsonObject = JSON.parse(msg.data);
+    var newPrice = parseFloat(jsonObject.c);
+    var finalPrice = newPrice.toFixed(2);
+    if(newPrice > lastPrice){
+      document.getElementById("u412-4").style.color = 'green';
+      document.getElementById("u412-4").textContent=finalPrice + " ⬆";
+      document.getElementById("u412-4").style.fontSize = 'large';
+    }
+    else {
+      document.getElementById("u412-4").style.color = 'red';
+      document.getElementById("u412-4").textContent=finalPrice + " ⬇";
+      document.getElementById("u412-4").style.fontSize = 'large';
+    }
+    lastPrice = newPrice;
+  };
+}
+
+updatePrice();
