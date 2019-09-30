d1cfddd4a6757bca2fc178f65f08b06687059ee1
Ethan Wilk <ethanwilk@gmail.com>
2019-09-29 18:33:28 -0700

    Update update.js

--- scripts/update.js
+++ scripts/update.js
@@ -2,8 +2,8 @@ function updatePrice(){
     var ws = new WebSocket('wss://stream.binance.com:9443/ws/btcusdt@miniTicker');
 
     ws.onmessage = function(msg) {
-      var newFloat = parseFloat(msg["c"])
-      console.log(newFloat)
+      var message = JSON.parse(msg);
+      var newFloat = parseFloat(message.c);
       var finalPrice = Number((newFloat).toFixed(2));
       document.getElementById("u412-4").textContent=finalPrice;
     };
