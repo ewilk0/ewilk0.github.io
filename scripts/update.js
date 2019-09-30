cf4262d5c7442bb6c27cdaa0efb2b02dbca1d594
Ethan Wilk <ethanwilk@gmail.com>
2019-09-29 19:48:30 -0700

    Update update.js

--- scripts/update.js
+++ scripts/update.js
@@ -10,7 +10,7 @@ function updatePrice(){
     document.getElementById("u412-4").textContent=finalPrice;
     if(finalPrice > lastPrice){
       document.getElementById("u412-4").style.color = 'green';
-      document.getElementById("u412-4").style.fontFamily = 'Courier';
+      document.getElementById("u412-4").style.fontFamily = 'Courier Bold';
     }
     else{
       document.getElementById("u412-4").style.color = 'red';
