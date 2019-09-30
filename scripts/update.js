0b0a65e7ca8a3f275447580c086cb33392ae27ce
Ethan Wilk <ethanwilk@gmail.com>
2019-09-29 17:42:36 -0700

    Update update.js

--- scripts/update.js
+++ scripts/update.js
@@ -4,7 +4,7 @@ function everySecond(){
 
 function newPrice(){
     var xmlhttp = new XMLHttpRequest();
-    var url = "https://api.binance.com/api/v1/ticker/price?symbol=BTCUSDT";
+    var url = "https://api.binance.com/api/v1/ticker/price?symbol=BTCUSDT:433";
     xmlhttp.onreadystatechange = function() {
       if (this.readyState == 4  &&  this.status == 200) {
         var json = JSON.parse(this.responseText);
