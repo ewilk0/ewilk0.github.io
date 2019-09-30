c6fdc20dae341f57ab54b8d2bce25d483b4a14dd
Ethan Wilk <ethanwilk@gmail.com>
2019-09-29 18:16:26 -0700

    Update update.js

--- scripts/update.js
+++ scripts/update.js
@@ -1,6 +1,4 @@
 function updatePrice(){
-    const WebSocket = require('ws');
-
     const ws = new WebSocket('wss://stream.binance.com:9443/ws/btcusdt@trade');
 
     ws.on('message', function incoming(data) {
