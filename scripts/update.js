41bc0a2f159d50684eebf61c48d915997f37b2ce
Ethan Wilk <ethanwilk@gmail.com>
2019-09-29 18:12:21 -0700

    Update update.js

--- scripts/update.js
+++ scripts/update.js
@@ -1,25 +1,7 @@
-function everySecond(){
-    setInterval(newPrice, 1000);
-}
+const WebSocket = require('ws');
 
-function newPrice(){
-    var xmlhttp = new XMLHttpRequest();
-    var url = "https://cors-anywhere.herokuapp.com/https://api.binance.com/api/v1/ticker/price?symbol=BTCUSDT";
-    xmlhttp.onreadystatechange = function() {
-      if (this.readyState == 4  &&  this.status == 200) {
-        var json = JSON.parse(this.responseText);
-        parseJson(json);
-      }		
-    };
-    xmlhttp.open("GET", url, false);
-    xmlhttp.setRequestHeader("Origin", 'api.binance.com');
-    xmlhttp.send();
-    function parseJson(json) {
-      var usdValue = json["price"];
-      var usdFloat = parseFloat(usdValue);
-      finalUSD = Number((usdFloat).toFixed(2));
-      console.log(finalUSD);
-      document.getElementById("u412-4").textContent=finalUSD;}}
+const ws = new WebSocket('wss://stream.binance.com:9443/ws/btcusdt@trade');
 
-everySecond();
-newPrice();
+ws.on('message', function incoming(data) {
+    console.log(data);
+});
